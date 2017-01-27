dd1cb0c2ea4e953d5228718e40098e0bb9d247fe
Max <Max@192.168.198.1>
2017-01-27 17:00:31 +0300

    del

--- plugins/ru.agentlab.wordnet.editor.ui/src/ru/agentlab/wordnet/editor/app/themes/DefaultTheme.java
+++
@@ -1,9 +0,0 @@
-package ru.agentlab.wordnet.editor.app.themes;
-
-import org.eclipse.fx.ui.theme.AbstractTheme;
-
-public class DefaultTheme extends AbstractTheme {
-	public DefaultTheme() {
-		super("theme.default", "Default theme", DefaultTheme.class.getClassLoader().getResource("css/default.css"));
-	}
-}
